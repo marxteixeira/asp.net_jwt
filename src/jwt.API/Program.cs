691b25a360970dadf612690b334c45b17dfcb95d
marxtts@gmail.com <95075808+marxteixeira@users.noreply.github.com>
2024-09-26 21:38:19 -0300

    adicionando policies

--- src/jwt.API/Program.cs
+++ src/jwt.API/Program.cs
@@ -1,15 +1,41 @@
+using asp.net_jwt;
 using asp.net_jwt.Models;
 using asp.net_jwt.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //resolver a dependência do TokenService
 builder.Services.AddTransient<TokenService>();
 //middleware de autenticação.
-builder.Services.AddAuthentication();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; //definir o esquema de autenticação padrão: JwtBearer (Bearer Token, JWT Bearer Token, OAuth 1.0 e etc…) 
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; //forma que ele vai interrogar a requisição para saber onde está o token: JwtBearer
+
+}).AddJwtBearer(x => //definir como e onde achar esse token, onde está a chave e como desencriptar esse token.
+{
+    //dentro desse objeto vamos definir como obter a chave e desemcriptar
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        //passando a chave que está nas configurações
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+        //vamos tirar essas validações, pois não estamos trabalhando com OIDC.
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+
 //middleware de autorização.
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(x => {
+    //vamos adicionar política para exigir o role admin
+    x.AddPolicy("Admin", p => p.RequireRole("admin"));
+});
 
 var app = builder.Build();
 
@@ -18,12 +44,25 @@ app.UseAuthentication();
 //adiciona autorização
 app.UseAuthorization();
 
-app.MapGet("/", (TokenService service) =>
+app.MapGet("/login", (TokenService service) =>
 {
     //Criar um novo e passar
     var user = new User("Marx Teixeira", 1, "email@marx.com", "https://balta.io", "sdfj", new[] { "student", "premium" });
 
     return service.Create(user);
 });
+
+app.MapGet("/restrito", (ClaimsPrincipal user) => new
+{
+    id = user.Claims.FirstOrDefault(x => x.Type == "id").Value,
+    name = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name).Value,
+    email = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email).Value,
+    givenName = user.Claims.FirstOrDefault(x => x.Type == ClaimTypes.GivenName).Value,
+    image = user.Claims.FirstOrDefault(x => x.Type == "image").Value,
+
+}).RequireAuthorization();
+
+//para testes o role admin
+app.MapGet("/admin", () => "Você tem acesso").RequireAuthorization("admin");
 
 app.Run();
